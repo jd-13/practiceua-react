460c8573033cd5455cc6a7d80d466126e279f26c
jd-13 <jackd8989@gmail.com>
2025-10-18 22:38:33 +0100

    Add tab view to homepage

--- practice-ua/app/index.tsx
+++ practice-ua/app/index.tsx
@@ -1,15 +1,63 @@
-import { Text, View } from "react-native";
+import * as React from 'react';
+import { Text, View, useWindowDimensions } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
+
+function AllLessonsRoute() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>All Lessons</Text>
+    </View>
+  );
+}
+
+function StarredLessonsRoute() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Starred Lessons</Text>
+    </View>
+  );
+}
+
+function RecentLessonsRoute() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Recent Lessons</Text>
+    </View>
+  );
+}
+
+function StaleLessonsRoute() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Stale Lessons</Text>
+    </View>
+  );
+}
+
+const renderScene = SceneMap({
+  first: AllLessonsRoute,
+  second: StarredLessonsRoute,
+  third: RecentLessonsRoute,
+  fourth: StaleLessonsRoute,
+});
+
+const routes = [
+  { key: 'first', title: 'All' },
+  { key: 'second', title: 'Starred' },
+  { key: 'third', title: 'Recent' },
+  { key: 'fourth', title: 'Stale' },
+];
 
 export default function Index() {
+  const layout = useWindowDimensions();
+  const [index, setIndex] = React.useState(0);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
   );
 }
