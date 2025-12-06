86d69ee8e94a424b265e39a1e34a76886b990dea
jd-13 <jackd8989@gmail.com>
2025-12-06 18:31:42 +0000

    Add missing path change

--- practice-ua/app/index.tsx
+++ practice-ua/app/index.tsx
@@ -1,4 +1,4 @@
-import { DatabaseProvider } from "@/lib/database/databaseContext";
+import { DatabaseProvider } from "@/lib/database/context";
 import * as React from "react";
 import { Text, View, useWindowDimensions } from "react-native";
 import { TabView, SceneMap } from "react-native-tab-view";
