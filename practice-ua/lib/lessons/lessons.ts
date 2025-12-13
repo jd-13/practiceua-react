076134d8b5a55b522b5d1697f26491bb800b7c62
jd-13 <jackd8989@gmail.com>
2025-12-13 01:16:48 +0000

    Fix lint errors

--- practice-ua/lib/lessons/lessons.ts
+++ practice-ua/lib/lessons/lessons.ts
@@ -1,10 +1,12 @@
-import {
-  LessonDefinition,
-  LessonCategory,
-} from "../lesson_core/definitions";
+import { LessonDefinition, LessonCategory } from "../lesson_core/definitions";
 import { buildGenerator, getData } from "./numbers/numbers";
 
-
 export const lessons: LessonDefinition[] = [
-  new LessonDefinition("Numbers", LessonCategory.Vocabulary, "numbers", buildGenerator, getData),
+  new LessonDefinition(
+    "Numbers",
+    LessonCategory.Vocabulary,
+    "numbers",
+    buildGenerator,
+    getData
+  ),
 ];
